94660bca0503b8c7e8efc4f838153c5f9fded502
Carter <carter.weidman@outlook.com>
2025-05-12 02:54:01 -0400

    playing with primevue, looking into auth

--- src/main.js
+++ src/main.js
@@ -2,9 +2,62 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 import router from './router'
+import { definePreset } from '@primevue/themes'
 
 const app = createApp(App)
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}',
+    },
+    formField: {
+      paddingX: '1rem',
+      paddingY: '1rem',
+    },
+    colorScheme: {
+      light: {
+        formField: {
+          hoverBorderColor: '{primary.color}',
+        },
+      },
+      dark: {
+        formField: {
+          hoverBorderColor: '{primary.color}',
+        },
+      },
+    },
+  },
+  components: {
+    button: {
+      paddingX: '.75rem',
+      paddingY: '.5rem',
+    },
+  },
+})
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      prefix: 'p',
+      cssLayer: false,
+    },
+  },
+})
 
 app.use(router)
 
