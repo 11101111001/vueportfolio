4fa972a1928af02abfe755d4ba37ef4d32e8764e
Carter <carter.weidman@outlook.com>
2025-05-22 14:27:28 -0400

    adding Auth0 for login functionality

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import App from './App.vue'
+import { createAuth0 } from '@auth0/auth0-vue'
 
 import './style.css'
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
-app.use(PrimeVue, {
-  theme: 'none',
-})
+app
+  .use(router)
+  .use(PrimeVue, {
+    theme: 'none',
+  })
+  .use(
+    createAuth0({
+      domain: import.meta.env.VITE_AUTH0_DOMAIN,
+      clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+      },
+    }),
+  )
 
 app.mount('#app')
